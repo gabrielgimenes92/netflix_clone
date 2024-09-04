abbd02c565387bc45e86e64a43a56ec680763f56
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-09-04 14:21:08 -0700

    First commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>netflix clone</h1>
+      </main>
+    </div>
+  );
+}
