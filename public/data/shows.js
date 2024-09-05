10e9bcd4d2c880276f5b2cf3d6b75964f7b68629
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-09-05 14:47:07 -0700

    Adds hovering effect on video

--- public/data/shows.js
+++ public/data/shows.js
@@ -15,7 +15,7 @@ export const linusTechTipsSection = {
       showBanner:
         'https://i.ytimg.com/vi/V0r1lr-4_SQ/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLAtZHaBlr1R9_2OqnyEo2OkDb70QQ',
       showVideo:
-        'https://i.ytimg.com/an_webp/V0r1lr-4_SQ/mqdefault_6s.webp?du=3000&sqp=CNSY47YG&rs=AOn4CLDi_tts6pSPXjmwzm0RI4l9WrcmsQ',
+        'https://i.ytimg.com/an_webp/V0r1lr-4_SQ/mqdefault_6s.webp?du=3000&sqp=CMyn6LYG&rs=AOn4CLDBsE9owWvs-nm0KbbUZdO4ZVMqhA',
       category: 'series',
       episodes: 8,
       tags: ['Excêntrico', 'Complexo', 'Humor ácido'],
--- src/app/components/Show.js
+++ src/app/components/Show.js
@@ -1,11 +1,35 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Show.module.scss';
 
 const Show = (showBanner, showVideo) => {
+  const [showVideoDisplay, setShowVideoDisplay] = useState(false);
+
   return (
-    <div>
-      <Image width={480} height={360} src={showBanner.showBanner} alt="" />
-      <Image width={320} height={180} src={showBanner.showVideo} alt="" />
+    <div
+      className={styles.show}
+      onMouseEnter={() => setShowVideoDisplay(true)}
+      onMouseLeave={() => setShowVideoDisplay(false)}
+    >
+      {showVideoDisplay ? (
+        <Image
+          className={styles.showVideo}
+          width={320}
+          height={180}
+          src={showBanner.showVideo}
+          alt=""
+        />
+      ) : (
+        <Image
+          className={styles.showBanner}
+          width={480}
+          height={360}
+          src={showBanner.showBanner}
+          alt=""
+        />
+      )}
     </div>
   );
 };
